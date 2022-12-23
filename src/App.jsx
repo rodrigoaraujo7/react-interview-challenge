28ca2f64904f96487ce9d64fba23c7abfb6794e0
Rodrigo Leite Araujo <dev.rodrigoaraujo@gmail.com>
2022-12-23 07:45:14 -0300

    mouse position & circle element

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { Circle } from './components/Circle';
 
 export const App = () => {
+  const [x, setX] = useState();
+  const [y, setY] = useState();
+
+  useEffect(() => {
+    const update = event => {
+      setX(event.x);
+      setY(event.y);
+    }
+
+    window.addEventListener('mousemove', update);
+    window.addEventListener('touchmove', update);
+  }, [setX, setY])
+
   return (
-    <h1>ReactJS Interview Challenge</h1>
+    <div>
+      <h1>ReactJS Interview Challenge</h1>
+      <h1>{`x: ${x}; y: ${y};`}</h1>
+      <Circle></Circle>
+    </div>
   )
 }
 
---
+++ src/components/Circle/index.jsx
@@ -0,0 +1,5 @@
+import './style.css'
+
+export const Circle = () => {
+    return <div className='circle'></div>
+}
