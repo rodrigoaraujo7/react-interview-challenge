fe0ce2d4c10513b6d2ada2359d2670ead56b3e16
Rodrigo Leite Araujo <dev.rodrigoaraujo@gmail.com>
2022-12-23 09:11:51 -0300

    new element in exactly position

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import { Circle } from './components/Circle';
 
 export const App = () => {
-  const [x, setX] = useState();
-  const [y, setY] = useState();
+  const [x, setX] = useState([]);
+  const [y, setY] = useState([]);
 
-  useEffect(() => {
-    const update = event => {
-      setX(event.x);
-      setY(event.y);
+  const handleClick = (event) => {
+    const newElementPosition = {
+      clientX: event.clientX,
+      clientY: event.clientY
     }
 
-    window.addEventListener('mousemove', update);
-    window.addEventListener('click', () => {
-      const element = document.createElement('span'); 
+    const element = document.createElement('span'); 
 
-      element.style.padding = '15px';
-      element.style.borderRadius = '80px';
-      element.style.backgroundColor = '#DA0037';
+    element.style.position = "absolute";
+    element.style.left = newElementPosition.clientX + 'px';
+    element.style.top = newElementPosition.clientY + 'px';
 
-      document.getElementById('content').appendChild(element)
-    });
-  }, [setX, setY])
+    element.style.padding = '15px';
+    element.style.borderRadius = '80px';
+    element.style.backgroundColor = '#DA0037';
+
+    console.log(newElementPosition.clientX)
+    console.log(newElementPosition.clientY)
+
+    document.getElementById('content').appendChild(element)
+  }
 
   return (
-    <div id='content'>
+    <div id='content' onClick={handleClick}>
       <h1>ReactJS Interview Challenge</h1>
-      <h1>{`x: ${x}; y: ${y};`}</h1>
+      {x && y ? (<h1>{`x: ${x}; y: ${y};`}</h1>) : null}
     </div>
   )
 }
